9524008f405614dc94c56cdecf186388fd9b2213
ov3rk1ll <overkillerror@gmail.com>
2026-03-30 11:55:50 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Forum&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import tl from "$lib/assets/tl-512.png";
+  import tr from "$lib/assets/tr-512.png";
+  import bl from "$lib/assets/bl-512.png";
+  import br from "$lib/assets/br-512.png";
+
+  const liste = new Array(5).fill(true).map((_, i) => ({
+    title: `Geschenk #${i + 1}`,
+    image: `https://placehold.co/600x400?text=Geschenk ${i + 1}`,
+    text: `Ein kurzer Text zu Geschenk #${i + 1} mit maximal zwei Zeilen auf Smartphone Größe`,
+  }))
+</script>
+
+<div class="relative min-h-screen w-full overflow-hidden bg-slate-50">
+  <img src={tl} alt="Corner Decoration" class="corner top-0 left-0" />
+
+  <img src={tr} alt="Corner Decoration" class="corner top-0 right-0" />
+
+  <img src={bl} alt="Corner Decoration" class="corner bottom-0 left-0" />
+
+  <img src={br} alt="Corner Decoration" class="corner bottom-0 right-0" />
+
+  <div
+    class="relative z-10 flex flex-col items-center justify-center min-h-screen py-16 px-8 lg:px-16 text-center"
+  >
+    <h1 class="lg:text-7xl text-5xl font-bold green uppercase">Geschenke</h1>
+    <h2 class="lg:text-5xl text-3xl green uppercase font-medium">
+      Katrin & Stephan
+    </h2>
+    <main class="text-lg text-slate-800 max-w-md space-y-8 mt-8">
+      {#each liste as item}
+        <section class="space-y-1">
+          <h3 class="lg:text-3xl text-xl green uppercase font-medium">
+            {item.title}
+          </h3>
+          <img src={item.image} alt={item.title} />
+          <p class="leading-tight">{@html item.text}</p>
+        </section>
+      {/each}
+    </main>
+  </div>
+</div>
+
+<style lang="scss">
+  @reference "tailwindcss";
+
+  .corner {
+    @apply fixed w-48 md:w-64 lg:w-96 opacity-40 z-20;
+  }
+</style>
