ee9b8c32a7bc1d9bb2d64ae57ecfcbb450336775
ov3rk1ll <overkillerror@gmail.com>
2026-04-02 19:19:29 +0200

    content

---
+++ src/routes/steiermark/+page.svelte
@@ -0,0 +1,29 @@
+<div
+  class="relative z-10 flex flex-col items-center justify-start min-h-screen py-16 px-8 lg:px-16 text-center"
+>
+  <h1 class="lg:text-7xl text-4xl font-bold green uppercase">
+    Katrin & Stephan
+  </h1>
+
+  <h2 class="lg:text-5xl text-3xl green uppercase font-medium">
+    26. Juli 2026
+  </h2>
+  <main class="text-lg text-slate-800 max-w-md space-y-0 mt-8">
+    <h3 class="lg:text-3xl text-xl green uppercase font-medium">Adresse</h3>
+    <p class="mb-8">
+      Schloss Kornberg<br />
+      Dörfl 2, 8330 Riegersburg<br />
+    </p>
+
+    <h3 class="lg:text-3xl text-xl green uppercase font-medium">Dresscode</h3>
+    <p class="mb-8">Sommerlich-Elegant-Dezent</p>
+    <h3 class="lg:text-3xl text-xl green uppercase font-medium">Geschenke</h3>
+    <p class="mb-8">
+      Euer Kommen ist für uns das schönste Geschenk!<br />Wenn ihr uns
+      zusätzlich eine Freude machen wollt, freuen wir uns über einen Beitrag zu
+      unseren geplanten Anschaffungen.<br />
+      Mehr Informationen zu unseren Wünschen findet ihr
+      <a href="/geschenke" target="_blank" class="green underline">hier</a>.
+    </p>
+  </main>
+</div>
